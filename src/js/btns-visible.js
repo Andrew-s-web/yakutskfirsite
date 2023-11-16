5ac2169240846487d03253346e0932f826a2837b
Andrew <63203237+Andrew-s-web@users.noreply.github.com>
2023-11-16 20:07:42 +0500

    btns

---
+++ src/js/btns-visible.js
@@ -0,0 +1,8 @@
+function switchVisible1() {
+        document.getElementById('ul1').style.display = 'none';
+        document.getElementById('ul2').style.display = 'block';
+}
+function switchVisible2() {
+    document.getElementById('ul2').style.display = 'none';
+    document.getElementById('ul1').style.display = 'block';
+}
