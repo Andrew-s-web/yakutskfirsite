ed612a553b25cedf0ba28f747f862dd897f8679e
Andrew <63203237+Andrew-s-web@users.noreply.github.com>
2023-12-07 19:40:37 +0500

    Update test.js

--- src/js/test.js
+++ src/js/test.js
@@ -1,4 +1,5 @@
-let miles = prompt('kilometers')
+/*let miles = prompt('kilometers')
 setTimeout(() => {
     alert(miles + " miles " + " = " + (miles * 1.6093).toFixed(2) + " kilometers")
 }, 5);
+*/
