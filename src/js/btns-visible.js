aa3b61618fe7bbd6f6cfd370e363991f575ec68b
Andrew <63203237+Andrew-s-web@users.noreply.github.com>
2024-01-14 14:04:42 +0500

    ss

--- src/js/btns-visible.js
+++ src/js/btns-visible.js
@@ -1,6 +1,6 @@
 function switchVisible1() {
-        document.getElementById('ul1').style.display = 'none';
-        document.getElementById('ul2').style.display = 'block';
+    document.getElementById('ul1').style.display = 'none';
+    document.getElementById('ul2').style.display = 'block';
 }
 function switchVisible2() {
     document.getElementById('ul2').style.display = 'none';
---
+++ src/js/table_switch.js
@@ -0,0 +1,4 @@
+function switchTableMagadan() {
+    document.getElementById('Yakutsk').style.display = 'none'
+    document.getElementById('Magadan').style.display = 'block'
+}
