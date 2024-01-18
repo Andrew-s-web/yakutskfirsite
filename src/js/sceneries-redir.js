fd0751a4ce90ac3510831b769eca5bcf76d0fe4d
Andrew <63203237+Andrew-s-web@users.noreply.github.com>
2024-01-18 17:55:29 +0500

    18.01.24

---
+++ src/js/sceneries-redir.js
@@ -0,0 +1,19 @@
+function redir_msfs() {
+    window.location.href = '../html/sceneries/msfs.html';
+}
+
+function redir_xplane11() {
+    window.location.href = '../html/sceneries/xplane11.html';
+}
+
+function redir_xplane12() {
+    window.location.href = '../html/sceneries/xplane12.html';
+}
+
+function redir_prepar3d() {
+    window.location.href = '../html/sceneries/prepar3d.html';
+}
+
+function redir_fs2004() {
+    window.location.href = '../html/sceneries/fs2004.html';
+}
